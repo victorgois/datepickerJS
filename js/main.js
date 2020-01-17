e3faedbd7131bdb8120ff10b4d0223b293b8f9b3
Victor Góis <victorgois18@gmail.com>
2020-01-17 19:22:54 -0300

    *

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//SELECTORS
+
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
@@ -5,7 +7,20 @@ const mth_element = document.querySelector('.date-picker .dates .month .mth');
 const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
 const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
-const images_element = document.querySelector('.event-images #img')
+/* 
+const title_element = document.querySelectorAll('#title');
+const description_element = document.querySelectorAll('#description');
+const image_element = document.querySelectorAll('#image');
+const start_element = document.querySelectorAll('#start');
+const end_element = document.querySelectorAll('#end');
+const recurrence_element = document.querySelectorAll('#recurrence');
+const costs_element = document.querySelectorAll('#costs');
+const link_element = document.querySelectorAll('#link');
+const venue_element = document.querySelectorAll('#venue');
+const category_element = document.querySelectorAll('#category');
+ */
+
+//const images_element = document.querySelector('.date-picker .dates .days #img')
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -19,26 +34,81 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-let image = document.querySelector('#img')
+let startDay = selectedDay;
+let startMonth = selectedMonth;
+let startYear = selectedYear;
+
+let eventsObj, timeStampArray = [];
+let eventsJson;
+
+
+/* let title = [];
+let description = [];
+let image = [];
+let start = [];
+let end = [];
+let recurrence = [];
+let costs = [];
+let link = [];
+let venue = [];
+let category = [];
+ */
 
 mth_element.textContent = months[month] + ' ' + year;
 
 selected_date_element.textContent = formatDate(date);
 populateDates();
 
+/* title_element.textContent = title;
+description_element.textContent = description;
+image_element.textContent = image;
+start_element.textContent = start;
+end_element.textContent = end;
+costs_element.textContent = costs;
+link_element.textContent = link;
+venue_element.textContent = venue;
+category_element.textContent = category; */
+
+
 //EVENT LISTENERS
 date_picker_element.addEventListener('click', toggleDatePicker);
 next_mth_element.addEventListener('click', goToNextMonth);
 prev_mth_element.addEventListener('click', goToPreviousMonth);
-images_element.addEventListener('click', showImage);
-
 
 //FUNCTIONS
 
-function showImage(e){
-    var img = document.querySelector('#img');
-    img.src = './img/avengers.jpg';
-    document.getElementById('img').appendChild(img);
+function getStartFromEvents(jsonObj){
+    let startArray = [];
+    for (var i = 0; i<jsonObj.length; i++){
+        startArray.push(jsonObj[i]['start']);
+    }
+    return (startArray);
+}
+
+function loadJson(){
+    
+    let startDateArray = [];
+    var requestURL = './sample-data.json'
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        eventsJson = request.response;
+        timeStampArray = getStartFromEvents(eventsJson['events']);//Array com timestamps
+        eventsJson = eventsJson['events'];//objeto com eventos
+        
+        startDateArray = validateDate(timeStampArray);// recebe um array com as datas convertidas
+        for (var i =0; i<startDateArray.length; i++){
+            if (startDateArray[i] <= selectedDate){
+                hideElements(i);
+            } else{
+                showElements(i);
+            }
+        }
+
+
+      }
 }
 
 function toggleDatePicker(e){
@@ -54,6 +124,7 @@ function goToNextMonth(e){
         year ++;
     }
     mth_element.textContent = months[month] + ' ' + year;
+    populateDates();
 }
 
 function goToPreviousMonth(e){
@@ -63,6 +134,8 @@ function goToPreviousMonth(e){
         year --;
     }
     mth_element.textContent = months[month] + ' ' + year;
+    populateDates();
+
 }
 
     //Function to populate the datepicker with dates
@@ -94,11 +167,64 @@ function populateDates(e){
             selected_date_element.dataset.value = selectedDate;
 
             populateDates();
+            loadJson();
+
+
         });
 
         days_element.appendChild(day_element);
     }
 }
+
+function validateDate(timestamp){
+    let start_timestamp;
+    let startDate = [];
+
+    for (var i = 0; i<timestamp.length; i++){
+        start_timestamp = timestamp[i];
+
+        startDate.push(new Date(start_timestamp*1000));
+
+    }
+    return startDate; //retorna um array com as datas convertidas
+
+}
+
+function hideElements(e){
+    var allOktoberfestByClass = document.getElementsByClassName('event-Oktoberfest');
+    var allAvengersByClass = document.getElementsByClassName('event-Avengers-Endgame');
+    var allRockImParkByClass = document.getElementsByClassName('event-RockimPark');
+    var allPCIMByClass = document.getElementsByClassName('event-PCIM-EUROPE');
+
+    var title_json = eventsJson[e]['title'];
+    if (title_json == 'RockimPark'){
+        allRockImParkByClass[0].style.display = 'none';
+    }
+    if (title_json == 'Oktoberfest'){
+        allOktoberfestByClass[0].style.display = 'none';
+    }
+    if (title_json == 'Avengers-Endgame'){
+        allAvengersByClass[0].style.display = 'none';
+    }
+    if (title_json == 'PCIM-EUROPE'){
+        allPCIMByClass[0].style.display = 'none';
+    }
+
+}
+
+function showElements(e){
+    var allOktoberfestByClass = document.getElementsByClassName('event-Oktoberfest');
+    var allAvengersByClass = document.getElementsByClassName('event-Avengers-Endgame');
+    var allRockImParkByClass = document.getElementsByClassName('event-RockimPark');
+    var allPCIMByClass = document.getElementsByClassName('event-PCIM-EUROPE');
+
+    allRockImParkByClass[0].style.display = 'block';
+    allOktoberfestByClass[0].style.display = 'block';
+    allAvengersByClass[0].style.display = 'block';
+    allPCIMByClass[0].style.display = 'block';
+
+}
+
 
 
 //HELPER FUNCTIONS
@@ -124,31 +250,3 @@ function formatDate (d){
 
     return day + ' / ' + month + ' / ' + year;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-$.getJSON( "ajax/test.json", function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-    });
-   
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
-  });
-
-  */
